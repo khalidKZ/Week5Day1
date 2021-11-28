eac1d5ff10482b8156ce555e04d31c7e09617477
khalidKZ <kz20201025@gmail.com>
2021-11-28 14:54:18 +0300

    test git

---
+++ app/src/main/java/com/example/week5day1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.week5day1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var xs = 1
+    }
+}
